8c651df1f53720a2fb0c28f01a38ed1dff149546
Alwin Yesudas <alwin11703@gmail.com>
2025-04-08 19:53:17 +0530

    Initial commit - Library Management System

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+
+# Create database tables
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def list_books():
+    books = Book.query.all()
+    return render_template('list.html', books=books)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        new_book = Book(title=title, author=author)
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('list_books'))
+    return render_template('add.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
